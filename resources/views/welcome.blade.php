fd8b9ea03a96a6211b51ba7c42d46826426e253d
Agus Tinus Turnip <tinusagus60@gmail.com>
2025-07-01 17:41:21 +0700

    ✨ feat(welcome): add meta description for SEO
    - add meta description tag to welcome.blade.php
    - improve SEO by providing a concise description of the page content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Portofolio Agus Tinus Turnip, seorang Full-Stack Developer yang berspesialisasi dalam Laravel, Tailwind CSS, dan pengembangan aplikasi mobile.">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
